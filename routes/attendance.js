f77653c5064c723b33c39f5a61d609337f063a1a
manoahLinks <manoahluka@gmail.com>
2024-08-17 01:11:50 +0100

    feat: add routes

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,11 +1,11 @@
 const router = require('express').Router(),
     controller = require('../controllers/attendance');
 
-router.route('/:email')
-    .get(controller.getAttendeeDetails)
-
 router.route('/')
     .get(controller.getAllAttendance)
     .post(controller.markAttendance)
+
+router.route('/:email')
+    .get(controller.getAttendeeDetails)
 
 module.exports = router;
