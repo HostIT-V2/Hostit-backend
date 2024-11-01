bd827d1e70ee0b57a5795cf0bc0fd17d048907a4
manoahLinks <manoahluka@gmail.com>
2024-11-01 02:28:17 +0100

    feat: integrate anambara

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -4,6 +4,7 @@ const Attendance = require('../models/attendance'),
     eventAbi = require('../abis/eventAbi.json'),
     nftAbi = require('../abis/nftAbi.json'),
     borderlessAbi = require('../abis/borderlessEventAbi.json'),
+    anambaraAbi = require('../abis/anabaraEventAbi.json'),
     fs = require('fs'),
     QRcode = require('qrcode');
 const VerifiedUser = require('../models/user');
@@ -346,6 +347,8 @@ exports.getAttendanceByEmail = async (req, res) => {
 
 // custom verifications
 
+// borderless
+
 exports.verifyAttendanceBorderless = async (req, res) => {
 
     let {email, day} = req.body;
@@ -399,6 +402,71 @@ exports.verifyAttendanceBorderless = async (req, res) => {
 
             // mark users attendance for that day
             let response = await Attendance.create({event: 'borderless', email: email, isPresent: true, day: day});
+
+            return res.status(200).json({message: 'success', data: response});
+
+        }
+
+    } catch (error) {
+        return res.status(400).json({message: 'failed to verify', error: error.message})
+    }
+}
+
+// Anambara techies
+
+exports.verifyAttendanceAnambaraTechies = async (req, res) => {
+
+    let {email, day} = req.body;
+
+    try {
+
+        let wallet = await ethers.Wallet.fromEncryptedJson(encryptedKey, process.env.PRIVATE_KEY_PASSWORD);
+
+        wallet = wallet.connect(provider);
+
+        // get smart contract instance with ethers
+        const eventContract = new ethers.Contract(process.env.LISK_CONTRACT_ADDRESS, anambaraAbi, wallet);
+
+        let foundUser = await VerifiedUser.findOne({event: 'anambara', email: email})
+
+       
+        if(!foundUser) {
+            // generate a wallet address
+            const response = await fetch(process.env.DYNAMIC_URL, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${process.env.DYNAMIC_TOKEN}`,
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({identifier: email, type:"email", chain :"EVM", socialProvider:"emailOnly"})
+            })
+
+            let data = await response.json();
+            
+            // save to User db
+            await VerifiedUser.create({event: 'anambara', email: email, address: data.user.walletPublicKey})
+                
+            // mark attendance 
+            const markAttTx = await eventContract.aw3c2024__markAttendance(data.user.walletPublicKey, day)
+
+            await markAttTx.wait()
+
+            // add to attendance collection
+            const markAtt = await Attendance.create({event: 'anambara', email: email, isPresent: true, day: day})
+
+            // return success and saved data
+            return res.status(201).json({message: 'successful', data: markAtt})
+
+        } else {
+
+            const transaction = await eventContract.aw3c2024__markAttendance(foundUser.address, day);
+
+            const reciept = await transaction.wait();
+
+            if(!reciept.status) throw Error("Attendance not marked on contract");
+
+            // mark users attendance for that day
+            let response = await Attendance.create({event: 'anambara', email: email, isPresent: true, day: day});
 
             return res.status(200).json({message: 'success', data: response});
 
